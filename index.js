eceac7c7de3069f7b84ec4e45abc12d2eeaf5481
BorisStankov98 <boris.stankov@kbc.com>
2025-03-18 16:41:05 +0200

    fixed the router issue

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/kate-voice-fe'));
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname +
-    '/kate-voice-fe/index.html'));
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname ,
+    'dist/kate-voice-fe/index.html'));
 });
 app.listen(process.env.PORT || 8080);
